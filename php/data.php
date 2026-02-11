c1287ecb0773814ef05a63e9422438d1b4d1c41e
SayanChowdhury29 <sayanchowdhury702@gmail.com>
2026-02-11 13:21:26 +0530

    First Commit

---
+++ php/data.php
@@ -0,0 +1,39 @@
+<?php
+    include_once "encryption.php"; // Include encryption functions at the top
+    
+    while($row = mysqli_fetch_assoc($query)){
+        $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
+                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+        $query2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_assoc($query2);
+        
+        if(mysqli_num_rows($query2) > 0){
+            // Decrypt the message
+            $result = decryptMessage($row2['msg']);
+        }else{
+            $result = "No message available";
+        }
+        
+        (strlen($result) > 28) ? $msg = substr($result, 0, 28) . '...' : $msg = $result;
+        
+        if(isset($row2['outgoing_msg_id'])){
+            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+        }else{
+            $you = "";
+        }
+        ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+        ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
+        
+        $output .= '<a href="chat.php?user_id='. $row['unique_id'] .'">
+                    <div class="content">
+                    <img src="php/images/'. $row['img'] .'" alt="">
+                    <div class="details">
+                        <span>'. htmlspecialchars($row['fname']. " " . $row['lname']) .'</span>
+                        <p>'. htmlspecialchars($you . $msg) .'</p>
+                    </div>
+                    </div>
+                    <div class="status-dot '. $offline .'"><i class="fas fa-circle"></i></div>
+                </a>';
+    }
+?>
